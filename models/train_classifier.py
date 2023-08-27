32718c5dfb361337ed0ff0409c5f7bcf14765af1
lightuse <lightuse@gmail.com>
2023-08-27 14:32:41 +0900

    implement ML process

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,41 +1,83 @@
 import sys
+import pandas as pd
+import numpy as np
+import sqlite3
+import sqlalchemy
+import matplotlib.pyplot as plt
+import nltk
+import re
+import lightgbm as lgb
+import pickle
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
+nltk.download(['punkt', 'wordnet'])
 
 def load_data(database_filepath):
-    pass
-
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table("disaster_messages", con=engine)
+    X = df['message']
+    y = df[df.columns[4:]]
+    return X, y, y.columns
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
 
+    tokens = word_tokenize(text)
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens=[]
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(lgb.LGBMClassifier()))
+    ])
+    parameter = {
+        'clf__estimator__n_estimators' : [50, 100]
+    }
+    cv = GridSearchCV(pipeline, param_grid=parameter, verbose=3)
 
+    return cv
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+def evaluate_model(model, X_test, y_test, category_names):
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=category_names))
 
 def save_model(model, model_filepath):
-    pass
-
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
-        model.fit(X_train, Y_train)
-        
+        model.fit(X_train, y_train)
+
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
